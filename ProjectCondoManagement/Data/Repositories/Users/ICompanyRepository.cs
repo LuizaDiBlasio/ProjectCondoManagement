e86caee66457d35a906b990ac99abc6dff2c40d1
LuizaDiBlasio <luizabandeira90@gmail.com>
2025-08-03 18:47:18 +0100

    Users Repository

---
+++ ProjectCondoManagement/Data/Repositories/Users/ICompanyRepository.cs
@@ -0,0 +1,9 @@
+﻿using ClassLibrary;
+using ProjectCondoManagement.Data.Entites.UsersDb;
+
+namespace ProjectCondoManagement.Data.Repositories.Users
+{
+    public interface ICompanyRepository : IGenericRepository<Company, DataContextUsers>
+    {
+    }
+}
